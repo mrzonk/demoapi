c659452860161ecd6a51de09f22723ecb42d1668
galang058 <galang058@gmail.com>
2023-05-20 01:42:20 +0700

    test buat deploy ke tomcat

--- src/main/java/com/domain/DemoapiApplication.java
+++ src/main/java/com/domain/DemoapiApplication.java
@@ -2,9 +2,10 @@ package com.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoapiApplication {
+public class DemoapiApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoapiApplication.class, args);
